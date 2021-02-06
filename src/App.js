a957574cb05d16e134cfc53093c530cb76dd8277
lilytao2001 <lily.tao0014@gmail.com>
2021-02-06 17:27:30 -0500

    add filter & search components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
-import React from 'react'; 
-import Display from './components/Display/Display';
-import { db } from './services/firebase'; 
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import Display from "./components/Display/Display";
+import { db } from "./services/firebase";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route,
+  withRouter,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import Search from "./components/Search";
+import BusinessPage from "./components/BusinessPage";
 
 class App extends React.Component {
   state = {
-    businesses: null
-  }
+    businesses: null,
+  };
 
-  // Run as soon as App on the screen 
-  componentDidMount(){
-    db.collection('Business')
+  // Run as soon as App on the screen
+  componentDidMount() {
+    db.collection("Business")
       .get()
-      .then( snapshot => {
-        const businesses = []
-        snapshot.forEach( doc => {
-          const data = doc.data()
-          businesses.push(data); 
-        })
-        this.setState({ businesses:businesses })
+      .then((snapshot) => {
+        const businesses = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          businesses.push(data);
+        });
+        this.setState({ businesses: businesses });
       })
-      .catch (error => console.log(error))
+      .catch((error) => console.log(error));
   }
 
-  render(){
+  render() {
     return (
-      <div className="App">
-        <h1>Frontline Deals</h1> 
-        {
-          this.state.businesses && 
-          <Display businesses={this.state.businesses}/>
-        }
-      </div>
-    )
+      <React.Fragment>
+        <Router>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/search" component={Search}></Route>
+          <Route
+            exact
+            path="/search/:business"
+            component={BusinessPage}
+          ></Route>
+        </Router>
+        {/* <div className="App">
+          <h1>Frontline Deals</h1>
+          {this.state.businesses && (
+            <Display businesses={this.state.businesses} />
+          )}
+        </div> */}
+      </React.Fragment>
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/components/Search.jsx
@@ -0,0 +1,184 @@
+import React, { Component } from 'react';
+import { db } from "../services/firebase";
+import { Container, Divider, Dropdown, Button, Icon } from 'semantic-ui-react';
+import {provOptions, categoryOptions} from "./Options";
+import CardsContainer from "./Cards/CardsContainer";
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            businesses: [],
+            province: "All",
+            category: "All",
+            byProv: false,
+            byCategory: false,
+        }
+    }
+    componentDidMount = async () => {
+        this.getStores(this.state.province, this.state.category, this.state.byProv, this.state.byCategory);
+    }
+    getStores = async (prov, category, byProv, byCategory) => {
+        if (byProv === true && byCategory === false) {
+            try {
+              const storesByName = await db
+                .collection("Business")
+                .where("Prov", "array-contains", prov)
+                .orderBy("Name")
+                .get()
+                .then((snapshot) => {
+                  const businesses = [];
+                  snapshot.forEach((doc) => {
+                    const data = doc.data();
+                    businesses.push(data);
+                  });
+                  this.setState({ businesses: businesses });
+                });
+        
+              return storesByName;
+            } catch (error) {
+              console.error(error);
+            }
+            return;
+          }
+        else if (byProv === false && byCategory === true) {
+            try {
+                const storesByName = await db
+                .collection("Business")
+                .where("Category", "==", category)
+                .orderBy("Name")
+                .get()
+                .then((snapshot) => {
+                  const businesses = [];
+                  snapshot.forEach((doc) => {
+                    const data = doc.data();
+                    businesses.push(data);
+                  });
+                  this.setState({ businesses: businesses });
+                });
+        
+              return storesByName;
+            } catch (error) {
+              console.error(error);
+            }
+            return;
+        }
+        else if (byProv === true && byCategory === true) {
+            try {
+                const storesByName = await db
+                .collection("Business")
+                .where("Prov", "array-contains", prov)
+                .where("Category", "==", category)
+                .orderBy("Name")
+                .get()
+                .then((snapshot) => {
+                  const businesses = [];
+                  snapshot.forEach((doc) => {
+                    const data = doc.data();
+                    businesses.push(data);
+                  });
+                  this.setState({ businesses: businesses });
+                });
+        
+              return storesByName;
+            } catch (error) {
+              console.error(error);
+            }
+            return;
+        }
+        else {
+            try {
+                const storesByName = await db
+                .collection("Business")
+                .orderBy("Name")
+                .get()
+                .then((snapshot) => {
+                  const businesses = [];
+                  snapshot.forEach((doc) => {
+                    const data = doc.data();
+                    businesses.push(data);
+                  });
+                  this.setState({ businesses: businesses });
+                });
+        
+              return storesByName;
+            } catch (error) {
+              console.error(error);
+            }
+            return;
+        }
+
+    }
+    onProvChange = (e, data) => {
+        this.setState({
+            province:data.value,
+        })
+        if (data.value != "All") {
+            this.setState ({
+                byProv: true,
+            })
+        }
+        else {
+            this.setState ({
+                byProv: false,
+            })
+        }
+    }
+    onCategoryChange = (e, data) => {
+        this.setState ({
+            category:data.value,
+        })
+        if (data.value != "All") {
+            this.setState ({
+                byCategory: true,
+            })
+        }
+        else {
+            this.setState ({
+                byCategory: false,
+            })
+        }
+    }
+    onSearch = () => {
+        this.getStores(this.state.province, this.state.category, this.state.byProv, this.state.byCategory);
+    }
+    render() { 
+        console.log(this.state);
+        return ( 
+            <React.Fragment>
+                <Container textAlign='center' style={{paddingTop: "5%"}}>
+                    <span>
+                        Show me deals in{' '}
+                        <Dropdown
+                            inline
+                            options={provOptions}
+                            defaultValue={provOptions[0].value}
+                            onChange={this.onProvChange}
+                        />
+                        for{' '}
+                        <Dropdown
+                            inline
+                            options={categoryOptions}
+                            defaultValue={categoryOptions[0].value}
+                            onChange={this.onCategoryChange}
+                        />
+                        {' '}
+                        <Button 
+                            style={{background:"white"}} 
+                            icon
+                            onClick={this.onSearch}
+                        >
+                            <Icon name='arrow right' />
+                        </Button>
+                    </span>
+                    <CardsContainer 
+                        businesses={this.state.businesses}
+                    />
+                </Container>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Search;
+

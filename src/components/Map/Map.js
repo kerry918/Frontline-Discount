53bff3a065aa9c547afac3ed575e73f4a9fe5913
Kerry Liu <kerryliukerui@gmail.com>
2021-02-06 23:30:04 -0500

    Added Header, and Included the Map component inside Business

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -26,41 +26,41 @@ export class Map extends React.Component {
         };
     }
 
-    componentDidMount(props) {
-        const location = "Tim Hortons"
-        const fields = "photos,formatted_address,name,opening_hours,rating,geometry"
-        const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="+ location +"&inputtype=textquery&fields="+ fields + "&locationbias=circle:2000@43.6532,-79.3832&key=AIzaSyDJQhRMFHH_zvZq47MDFoiNKqPOGXg2P6A"
-        fetch(proxyurl + url)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true, 
-                        location: result.candidates,
-                        lat: result.candidates.geometry.location.lat, 
-                        lng: result.candidates.geometry.location.lng
-                    }); 
-                }, 
-                (error) => {
-                    this.setState({
-                        isLoaded: true, 
-                        location: error
-                    })
-                }
-            )
-    }
+    // componentDidMount(props) {
+    //     const location = "Tim Hortons"
+    //     const fields = "photos,formatted_address,name,opening_hours,rating,geometry"
+    //     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    //     const url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="+ location +"&inputtype=textquery&fields="+ fields + "&locationbias=circle:2000@43.6532,-79.3832&key=AIzaSyDJQhRMFHH_zvZq47MDFoiNKqPOGXg2P6A"
+    //     fetch(proxyurl + url)
+    //         .then(res => res.json())
+    //         .then(
+    //             (result) => {
+    //                 this.setState({
+    //                     isLoaded: true, 
+    //                     location: result.candidates,
+    //                     lat: result.candidates.geometry.location.lat, 
+    //                     lng: result.candidates.geometry.location.lng
+    //                 }); 
+    //             }, 
+    //             (error) => {
+    //                 this.setState({
+    //                     isLoaded: true, 
+    //                     location: error
+    //                 })
+    //             }
+    //         )
+    // }
     
 
     render(props){
         console.log(this.state.location); 
         // console.log(this.state.lat); 
         const { error, location, isLoaded, lat, lng } = this.state; 
-        if (error) {
-            return <div>Error: {error.message}</div>
-        } else if (!isLoaded) {
-            return <div>Loading ... </div>
-        } else {
+        // if (error) {
+        //     return <div>Error: {error.message}</div>
+        // } else if (!isLoaded) {
+        //     return <div>Loading ... </div>
+        // } else {
             return (
                 <div>
                     <GoogleMap 
@@ -104,7 +104,7 @@ export class Map extends React.Component {
                     </GoogleMap>
                 </div>
             ); 
-        }
+        // }
     }
 }
 

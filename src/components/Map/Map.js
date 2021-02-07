26fc9dec369059cdc1e4a76248eb786b226d23cd
Kerry Liu <kerryliukerui@gmail.com>
2021-02-07 14:11:53 -0500

    Final Updates

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,8 +4,8 @@ import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from "react-
 import mapStyles from "./mapStyles"; 
 
 const center = {
-    lat: 43.6532,
-    lng: -79.3832,
+    lat: 43.8561,
+    lng: -79.3370,
 };
 
 const options = {
@@ -26,53 +26,53 @@ export class Map extends React.Component {
         };
     }
 
-    // componentDidMount(props) {
-    //     const location = "Tim Hortons"
-    //     const fields = "photos,formatted_address,name,opening_hours,rating,geometry"
-    //     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    //     const url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="+ location +"&inputtype=textquery&fields="+ fields + "&locationbias=circle:2000@43.6532,-79.3832&key=AIzaSyDJQhRMFHH_zvZq47MDFoiNKqPOGXg2P6A"
-    //     fetch(proxyurl + url)
-    //         .then(res => res.json())
-    //         .then(
-    //             (result) => {
-    //                 this.setState({
-    //                     isLoaded: true, 
-    //                     location: result.candidates,
-    //                     lat: result.candidates.geometry.location.lat, 
-    //                     lng: result.candidates.geometry.location.lng
-    //                 }); 
-    //             }, 
-    //             (error) => {
-    //                 this.setState({
-    //                     isLoaded: true, 
-    //                     location: error
-    //                 })
-    //             }
-    //         )
-    // }
+    componentDidMount(props) {
+        const location = "Tim Hortons"
+        const fields = "photos,formatted_address,name,opening_hours,rating,geometry"
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="+ location +"&inputtype=textquery&fields="+ fields + "&locationbias=circle:2000@43.6532,-79.3832&key=API_KEY"
+        fetch(proxyurl + url)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true, 
+                        location: result.candidates,
+                        lat: result.candidates.geometry.location.lat, 
+                        lng: result.candidates.geometry.location.lng
+                    }); 
+                }, 
+                (error) => {
+                    this.setState({
+                        isLoaded: true, 
+                        location: error
+                    })
+                }
+            )
+    }
     
 
     render(props){
         console.log(this.state.location); 
-        // console.log(this.state.lat); 
+        console.log(this.state.lat); 
         const { error, location, isLoaded, lat, lng } = this.state; 
-        // if (error) {
-        //     return <div>Error: {error.message}</div>
-        // } else if (!isLoaded) {
-        //     return <div>Loading ... </div>
-        // } else {
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <div>Loading ... </div>
+        } else {
             return (
                 <div>
                     <GoogleMap 
-                        defaultZoom={12}
+                        defaultZoom={14}
                         defaultCenter={center}
                         defaultOptions={options}
                     >
                         <Marker 
                             key={1}
                             position={{
-                                lat:43.6532, 
-                                lng: -79.3832
+                                lat:43.86826, 
+                                lng: -79.31207
                             }}
                             onClick={() => {
                                 this.setState({
@@ -84,8 +84,8 @@ export class Map extends React.Component {
                         {this.state.selectedPark && (
                             <InfoWindow
                                 position={{
-                                    lat: 43.6532, 
-                                    lng: -79.3832
+                                    lat: 43.86826, 
+                                    lng: -79.31207
                                 }}
                                 onCloseClick={() => {
                                     this.setState({
@@ -103,7 +103,7 @@ export class Map extends React.Component {
                     </GoogleMap>
                 </div>
             ); 
-        // }
+        }
     }
 }
 
--- src/components/Map/WrappedMap.js
+++ src/components/Map/WrappedMap.js
@@ -6,7 +6,7 @@ export default function MapMain(props) {
         <div style={{height: '90vh', width: '80vw', margin: '10%'}}>
             <WrappedMap 
                 googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
-                libraries=geometry,drawing,places&key=AIzaSyA0V29N5BoeQR7WmmJ0D3C8qe6acXqO8uk`}
+                libraries=geometry,drawing,places&key=API_KEY`}
                 loadingElement={<div style={{height: "100%"}}/>}
                 containerElement={<div style={{height: "100%"}}/>}
                 mapElement={<div style={{height: "100%"}}/>}
